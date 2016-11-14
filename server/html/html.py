7a97e19ea41e7c620f75c179496c2c27e1aa3e37
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2016-11-14 22:34:37 +0100

    GET parameters in server

--- server/html/html.py
+++ server/html/html.py
@@ -1,16 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from gpiozero import LED
+from time import sleep
 from subprocess import call
 
 app = Flask(__name__)
 
+led = LED(11)
+
 dict = {'result': 'ok'}
+dictChambreNumero = {'chambreParents': '45476341', 'salon4': '86545832', 'salon3': '78254586', 'bureau': '15789437', 'chambreGE': '549873', 'relais': '12325261'}
+	
 @app.route("/on")
 def on():
-	call(["/usr/local/bin/send", "8", "12325261", "1", "on"])
+	chambre = request.args.get("salle")
+	numero = dictChambreNumero[chambre]
+	call(["/usr/local/bin/send", "8", numero, "1", "on"])
 	return jsonify(**dict)
 @app.route("/off")
 def off():
-	call(["/usr/local/bin/send", "8", "12325261", "1", "off"])
+	chambre = request.args.get("salle")
+	numero = dictChambreNumero[chambre]
+	call(["/usr/local/bin/send", "8", numero, "1", "off"])
 	return jsonify(**dict)
 if __name__ == "__main__":
     app.run()
