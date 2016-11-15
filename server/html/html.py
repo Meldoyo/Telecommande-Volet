6579b415f61dc1195e600772f362539a0a4cbb76
Paul-Adrien Cordonnier <pa.cordonnier@gmail.com>
2016-11-16 00:59:06 +0100

    Modify server

--- server/html/html.py
+++ server/html/html.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from time import sleep
 from subprocess import call
 from json import dumps
@@ -6,10 +6,11 @@ from json import dumps
 app = Flask(__name__)
 
 dict = {'result': 'ok'}
-dictChambreNumero = {'chambreParents': '45476341', 'salon4': '86545832', 'salon3': '78254586', 'bureau': '15789437', 'chambreGE': '549873', 'relais': '12325261'}
+dictChambreNumero = {'chambreParents': '45476341', 'salon4': '557862', 'salon3': '78254586', 'bureau': '15789437', 'chambreGE': '549873', 'relais': '12325261'}
+dictChambreNom = {'chambreParents': 'Chambre Parents', 'salon4': 'Salon Grand', 'salon3': 'Salon Petit', 'bureau': 'Bureau', 'chambreGE': 'Chambre GE', 'relais': 'Relais'}
 array = []
-for key, value in dictChambreNumero.iteritems():
-	array.append({'name': key, 'id': value})
+for key, value in dictChambreNom.iteritems():
+	array.append({'name': key, 'common_name': value})
 
 	
 @app.route("/on")
@@ -22,10 +23,11 @@ def on():
 def off():
 	chambre = request.args.get("salle")
 	numero = dictChambreNumero[chambre]
+	print 'hello'
 	call(["/usr/local/bin/send", "8", numero, "1", "off"])
 	return jsonify(**dict)
 @app.route("/get")
 def get():
-	return dumps(array)
+	return Response(dumps(array), mimetype='application/json')
 if __name__ == "__main__":
     app.run()
